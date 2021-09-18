3e6a2bcd4947dd390c52642a08c3edd58b4e0ae8
AntiPrim3 <amit.rockinad.das@gmail.com>
2021-09-18 19:39:19 +0530

    Added More Readme Content

--- playgame/index.js
+++ playgame/index.js
@@ -135,8 +135,87 @@ function evaluateGameState(row, column, user) {
 }
 
 function updateReadmeFromGamestate(lastMoveResultMessage = "") {
-  let readmeString = `## **❌ Tic Tac Toe in Readme ⭕**
-### ***🎮 Game is in Progress.*** 
+  let readmeString = `## Namaste 🙏. I'm Amit, a Front-End Developer from India <img src="https://upload.wikimedia.org/wikipedia/en/thumb/4/41/Flag_of_India.svg/1200px-Flag_of_India.svg.png" width=35/>
+
+  ### 📖 A little more about me...
+  \`\`\`javascript
+  import { Human } from '@earth/animals';
+  import { Days } from 'calendar';
+  
+  class Amit extends Human {
+    constructor(day) {
+      this.today = day;
+      this.age = 26;
+          this.familiarWith = [\`JavaScript\`, \`React\`, \`Node\`, \`CSS\`, \`Core Java\`, \`SQL\`];
+          this.learning = [\`TypeScript\`, \`Redux\`, \`Jest\`, \`Webpack\`, \`MongoDB\`, \`YAML\`, \`Material UI\`];
+          this.hobbies = [\`Coding\`, \`Gaming\`, \`Learning new Tech\`];
+      this.academicHistory = [
+        {
+          institution: \`St. Xavier's Institution\`,
+                  passout: \`2011\`,
+                  specialization: \`N/A\`,
+                  location: \`Kolkata, West Bengal\`,
+                  degree: \`Secondary\`,
+        },
+        {
+          institution: \`St. Xavier's Institution\`,
+                  passout: \`2013\`,
+                  location: \`Kolkata, West Bengal\`,
+                  specialization: \`Science\`,
+                  degree: \`Higher Secondary\`,
+        },
+        {
+          institution: \`University of Engineering & Management\`,
+                  passout: \`2017\`,
+                  location: \`Jaipur, Rajasthan\`,
+                  specialization: \`Computer Science\`,
+                  degree: \`B.Tech\`,
+        },
+      ];
+      this.professionalHistory = [
+        {
+          name: \`Infosys Limited\`,
+          started: \`Oct, 2017\`,
+          ended: \`Jun, 2019\`,
+          location: \`Bangalore, Karnataka\`,
+          designation: \`Systems Engineer\`
+        },
+        {
+          name: \`Labvantage Solutions\`,
+          started: \`Jun, 2019\`,
+          ended: null,
+          location: \`Kolkata, West Bengal\`,
+          designation: \`Solutions Engineer\`
+        },
+      ];
+    }
+      live() {
+          if([Days.SUNDAY, Days.MONDAY].includes(this.day)) {
+              this.wakeUp();
+              this.game();
+              this.eat();
+              this.learn();
+              this.sleep();
+              return;
+          }
+          this.wakeUp();
+          this.work();
+          this.eat();
+          this.work();
+          this.sleep();
+      }
+  }
+  
+  let today = new Date();
+  while(today !== Days.END) {
+      (new Amit(today)).live();
+  }
+  \`\`\`
+  
+  ---
+
+### **❌ Tic Tac Toe in Readme ⭕**
+ 
 \
 🖱️ Just Click on Any of the Blank Squares below to place an ${resolveSymbolorIssue(gameState.symbol)}.
   
